b6bd684652e756e883994717a40327331f3e62af
Isaac Mills-Owoo <poppymills2@gmail.com>
2025-04-02 17:13:30 -0400

    Passed all test except IllegalArgumentException, will work on it

--- src/main/zipcode/Hamming.java
+++ src/main/zipcode/Hamming.java
@@ -1,10 +1,25 @@
 package zipcode;
 
 public class Hamming {
+    String s;
+    String s1;
+
     public Hamming(String s, String s1) {
+        this.s = s;
+        this.s1 = s1;
     }
 
-    public int getHammingDistance() {
-        return -1;
+    public int getHammingDistance() throws IllegalArgumentException {
+        int count = 0;
+        if (s.length() != s1.length()) {
+            throw new IllegalArgumentException("leftStrand and rightStrand must be of equal length.");
+        } else {
+            for (int i = 0; i < s.length(); i++) {
+                if (s.charAt(i) != s1.charAt(i)) {
+                    count++;
+                }
+            }
+        }
+        return count;
     }
 }
